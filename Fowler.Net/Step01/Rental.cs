ccaf6b54edee9d585e758ea95abd0edc87fe1366
Michał Poręba <michalporeba@gmail.com>
2021-02-19 23:07:47 +0000

    first attempt at translating the original java example

---
+++ Fowler.Net/Step01/Rental.cs
@@ -0,0 +1,14 @@
+﻿namespace Step01
+{
+    public class Rental
+    {
+        public Movie Movie { get; private set; }
+        public int DaysRented { get; private set; }
+
+        public Rental(Movie movie, int daysRented)
+        {
+            this.Movie = movie;
+            this.DaysRented = daysRented;
+        }
+    }
+}
